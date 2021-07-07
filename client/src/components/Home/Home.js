1041c2652fc36191653c632119ce364171ff1eb5
Carla Maceiras <carla.maceiras@gmail.com>
2021-07-07 09:35:32 +0200

    fotos

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,34 +1,44 @@
 import "../Home/home.css";
-import surf from "../../images/escuela.jpg"
-import sup from "../../images/sup.png"
+import surf from "../../images/costa.jpg"
+import sup from "../../images/sup2.jpg"
 import kayak from "../../images/kayak.png"
+import video from "../../images/Surfing.mp4"
 
 
 const Home = () => {
 
     return (
         
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={surf} className="d-block w-100" alt="logo"/>
+        <div className="separacion">
+            <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src={surf} className="d-block w-100" alt="logo"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={sup} className="d-block w-100" alt="logo"/>
+                    </div>
+                    <div className="carousel-item">
+                        <img src={kayak} className="d-block w-100" alt="logo"/>
+                    </div>
                 </div>
-                <div className="carousel-item">
-                    <img src={sup} className="d-block w-100" alt="logo"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={kayak} className="d-block w-100" alt="logo"/>
-                </div>
+                {/* <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span> 
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span> 
+                </button> */}
+
+                {/* <div class="youtube">
+
+                    <video src={video} width="400"  autoplay muted loop></video>
+
+                </div> */}
             </div>
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
         </div>
+
     )
     
 }
